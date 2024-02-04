fa0d2c35caf2b7dbec781ddfa511fb33bf579853
ALMLAMBO <aleksm007148@gmail.com>
2024-02-04 16:45:26 +0200

    Update models, configuration and create a controller for handling all logic for questions

---
+++ TestGenerator/TestGeneratorApi/QuestionController.cs
@@ -0,0 +1,46 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestGeneratorModels.DB;
+using TestGeneratorServices;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace TestGeneratorApi {
+	[ApiController]
+	[Route("api/[controller]/[action]")]
+	public class QuestionController : ControllerBase {
+		private readonly QuestionService _questionService;
+		private readonly int _questionCount;
+		
+		public QuestionController(QuestionService questionService) {
+			this._questionCount = 12;
+			this._questionService = questionService;
+		}
+		
+		[HttpGet]
+		public List<Question> GetAllQuestions() {
+			return _questionService.GetAllQuestions();
+		}
+		
+		[HttpPost]
+		public IActionResult AddQuestion([FromBody] JsonElement jsonElement) {
+			Question question = JsonConvert.DeserializeObject<Question>(jsonElement.GetRawText())!;
+			_questionService.AddQuestion(question);
+
+			return this.Ok("Question added successfully!");
+		}
+
+		[HttpGet]
+		public List<Question> GenerateQuiz() {
+			return this._questionService.GenerateQuiz(this._questionCount);
+		}
+		
+		[HttpPost]
+		public int EvaluateQuiz([FromBody] JsonElement jsonElement) {
+			Dictionary<string, string> answers = 
+				JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonElement.GetRawText())!;
+			
+			return this._questionService.EvaluateQuiz(this._questionCount, answers);
+		}
+	}
+}
--- TestGenerator/TestGeneratorModels/DB/Answer.cs
+++ TestGenerator/TestGeneratorModels/DB/Answer.cs
@@ -1,13 +1,6 @@
 namespace TestGeneratorModels.DB {
 	public class Answer {
-		public string AnswerText { get; set; }
+		public string? AnswerText { get; set; }
 		public bool Correct { get; set; }
-
-		public Answer() { }
-
-		public Answer(string answerText, bool correct) {
-			AnswerText = new string(answerText);
-			Correct = correct;
-		}
 	}
 }
